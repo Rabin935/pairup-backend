e25e48eba03494901b6da802eaba5c912697e8e4
Rabin Tamang <lrabin935@gmail.com>
2026-01-02 14:46:23 +0545

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const PORT: number = 
+process.env.PORT ? parseInt(process.env.PORT): 3000;
+
+export const MONGO_URI: string =
+process.env.MONGO_URI || 'mongodb+srv://ttrabin935_db_user:DW6AqoVctjtKXeND@cluster0.92eh3by.mongodb.net/test';
+
+export const JWT_SECRET: string =
+    process.env.JWT_SECRET || 'defaultsecret';
