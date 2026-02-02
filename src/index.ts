3eed575233ca880defca803894a1f323607de7a5
Rabin Tamang <lrabin935@gmail.com>
2026-02-02 20:50:07 +0545

    added

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import express , { Application, Request, Response } from 'express';
+import  bookRoutes  from './routes/book.route';
+import { connectDB } from './database/mongodb';
+import bodyParser from 'body-parser';
+import { PORT } from './config';
+
+import authRoutes from './routes/auth.routes';
+
+
+
+
+// dotenv.config();
+
+const app: Application = express();
+// const PORT: number = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+
+
+
+
+app.use('/api/books', bookRoutes); 
+app.use('/api/auth', authRoutes);
+
+
+
+
+async function startServer() {
+    await connectDB();
+    app.listen(
+    PORT, 
+    () => {
+        console.log(`Server on http://localhost:${PORT}`);
+    }
+);
+}
+
+startServer();
+
