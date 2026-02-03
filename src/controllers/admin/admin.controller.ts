080ee26e079738f15a991e627f16414ccf921607
Rabin Tamang <lrabin935@gmail.com>
2026-02-03 10:28:26 +0545

    updated admin controller

--- src/controllers/admin/admin.controller.ts
+++ src/controllers/admin/admin.controller.ts
@@ -12,3 +12,31 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
 
  res.json(user);
 };
+
+export const getUsers = async (req: Request, res: Response) => {
+ const users = await User.find();
+ res.json(users);
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+ const user = await User.findById(req.params.id);
+ res.json(user);
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+ const data = req.body;
+
+ if(req.file){
+   data.image = req.file.path;
+ }
+
+ const user = await User.findByIdAndUpdate(req.params.id, data, { new: true });
+
+ res.json(user);
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+ const user = await User.findByIdAndDelete(req.params.id);
+ res.json(user);
+};
+
