e981829afb680002d3b9885fe43c954f6c3a88ac
Rabin Tamang <lrabin935@gmail.com>
2026-02-03 10:19:39 +0545

    added admin route

--- src/routes/admin/admin.route.ts
+++ src/routes/admin/admin.route.ts
@@ -1,26 +1,14 @@
 import express from "express";
-import { protect } from "../../middleware/authorized.middleware";
+import { authorizedMiddleware } from "../../middleware/authorized.middleware";
 import { isAdmin } from "../../middleware/admin.middleware";
 import { upload } from "../../middleware/multer";
 
 import {
- createUser,
- getUsers,
- getUserById,
- updateUser,
- deleteUser
+ createUser
 } from "../../controllers/admin/admin.controller";
 
 const router = express.Router();
 
-router.post("/users", protect, isAdmin, upload.single("image"), createUser);
-
-router.get("/users", protect, isAdmin, getUsers);
-
-router.get("/users/:id", protect, isAdmin, getUserById);
-
-router.put("/users/:id", protect, isAdmin, upload.single("image"), updateUser);
-
-router.delete("/users/:id", protect, isAdmin, deleteUser);
+router.post("/users", authorizedMiddleware, isAdmin, upload.single("image"), createUser);
 
 export default router;
